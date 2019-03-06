b97379f7c895c96a6d1c0fc61daa0086b8d1c079
David Soldevila Puigbi <david.soldevila.puigbi@a5s104pc22.fib.upc.es>
2019-03-06 09:57:58 +0100

    update

--- lab2/LAB2/SUMA/SUM4/CODIGO/S4bits.vhd
+++ lab2/LAB2/SUMA/SUM4/CODIGO/S4bits.vhd
@@ -11,21 +11,30 @@ use work.all;
 -- Los componentes se conectan mediante señales		
 		
 entity S4bits is 		
-generic(ret_xor: time := 15 ns;
+generic(
+	ret_xor: time := 15 ns;
 	ret_and: time := 10 ns;
 	ret_or: time := 15 ns);
-port (A: in std_logic_vector(3 downto 0);
+port (
+	A: in std_logic_vector(3 downto 0);
 	B: in std_logic_vector(3 downto 0);
 	cen: in	std_logic;
 	SUM: out std_logic_vector(3 downto 0);
 	csal: out std_logic);
 end S4bits;		
-		
-architecture estructural of S4bits is		
--- componentes y senyales 
+				
 
+architecture estructural of S4bits is
+component S1bit
+	port (x, y, cen : in std_logic; s, csal : out std_logic);
+end component;
+
+signal c1, c2, c3, c4: std_logic;
 begin
--- instanciacion e interconexiones		
-
-end estructural;		
+	S1bit0: S1bit port map(x=>A(0), y=>B(0), cen=>cen, csal=>c1, s=>SUM(0));
+	S1bit1: S1bit port map(x=>A(1), y=>B(1), cen=>c1, csal=>c2, s=>SUM(1));
+	S1bit2: S1bit port map(x=>A(2), y=>B(2), cen=>c2, csal=>c3, s=>SUM(2));
+	S1bit3: S1bit port map(x=>A(3), y=>B(3), cen=>c3, csal=>c4, s=>SUM(3));
+	csal <= c4;
+end estructural;
 
